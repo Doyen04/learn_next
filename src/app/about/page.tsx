f23a105f4fa47cf4d50f1a40970a3c248dcc7c7d
Doyen <101886678+Doyen04@users.noreply.github.com>
2024-11-13 23:15:40 +0100

    starting point of my next lesson

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+            fff
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import style from '@/styles/header.module.css'
+
+
+function Header(){
+    return(
+        <header className={style.header}>
+            <div>About</div>
+            <div>Sign up</div>
+            <div></div>
+            <div></div>
+        </header>
+    )
+}
+
+export default Header
