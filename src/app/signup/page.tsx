be80c9da95d732b21c454529cd4fe2750429bda6
Doyen <101886678+Doyen04@users.noreply.github.com>
2024-11-14 14:22:27 +0100

    signup page was added

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import Link from 'next/link';
+import style from '@/styles/signup_page.module.css'
 
-function Signup(){
-    return(
-        <div>
-            fff
-        </div>
+
+function Signup() {
+    return (
+        <section className={style.signup_page}>
+            <div className={style.form_container}>
+                <h1 className={style.form_head}>Sign Up</h1>
+                <form action="" method="post" className={style.form}>
+                    <input type="email" name="email" id="email" placeholder="Email" className={style.input} />
+                    <input type="password" name="password" id="password" placeholder="Password" className={style.input} />
+                    <input type="button" value="Submit" className={`${style.input} ${style.submit}`} />
+                    <Link href=""></Link>
+                </form>
+                <div className={style.line_with_text}>
+                    <span>OR</span>
+                </div>
+                <input type="button" value="Google" className={`${style.input} ${style.google_github_auth}`} />
+                <input type="button" value="Github" className={`${style.input} ${style.google_github_auth}`} />
+            </div>
+        </section>
     )
 }
 
