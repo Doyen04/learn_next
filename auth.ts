e2526d064034ef493ca045a577a6e31977300363
Doyen <101886678+Doyen04@users.noreply.github.com>
2024-11-21 20:22:55 +0100

    added a prisma.ts file to reduce unnecessary import of prisma

--- auth.ts
+++ auth.ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "~@/lib/prisma"
 
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
-import { Prisma } from "@prisma/client"
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-    adapter: PrismaAdapter(Prisma),
+    adapter: PrismaAdapter(prisma),
     providers: [GitHub, Google],
+    //add a call back to add overlapping email
 })
