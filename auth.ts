a6e3944c9f5d3a62958a3440fa4e25bbd947dfea
Doyen <101886678+Doyen04@users.noreply.github.com>
2024-11-21 17:28:51 +0100

    added schema for database

--- auth.ts
+++ auth.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import { Prisma } from "@prisma/client"
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+    adapter: PrismaAdapter(Prisma),
     providers: [GitHub, Google],
 })
